5c0aeee9a3524134a84b273681619c473caeca33
Oliver Carter <oliverc101@hotmail.com>
2018-12-06 18:23:55 +0000

    Finished website

--- exercise2/cgi-bin/easter.py
+++ exercise2/cgi-bin/easter.py
@@ -2,8 +2,10 @@
 import cgi, cgitb
 cgitb.enable() # displays any errors
 form = cgi.FieldStorage()
-x = form.getvalue("datepicker")
-a = int(x) % 19
+x = int(form.getvalue("datepicker"))
+format = form.getvalue("format")
+
+a = x % 19
 b = x // 100
 c = x % 100
 d = b // 4
@@ -21,15 +23,66 @@ n = (h - m + r + 90) // 25
 p = (h - m + r + n + 19) % 32
   #calculates the day of easter
 
+sup = str()
+if len(str(p)) == 1:
+    if str(p) == '1':
+        sup = 'st'
+    elif str(p) == '2':
+        sup = 'nd'
+    elif str(p) == '3':
+        sup = 'rd'
+    else:
+        sup = 'th'
+elif len(str(p)) == 2:
+    if str(p)[0] == '1':
+        sup = 'th'
+    elif str(p)[1] == '1':
+        sup = 'st'
+    elif str(p)[1] == '2':
+        sup = 'nd'
+    elif str(p)[1] == '3':
+        sup = 'rd'
+    else:
+        sup = 'th'
+        #   calculates what the superscript will be based off
+        #   the value the day
+month = str()
+
+if n == 3:
+    month='March'
+elif n == 4:
+    month='April'
+        #   calculates the verbal format of the month
+
 print('Content-Type: text/html; charset=utf-8')
 print('')
 print('<!DOCTYPE html>')
-print('<html>')
-print('<head> <title> Finding Easter </title>  </head>')
+print('<html lang="en" style="background-color: #23272a">')
+print('<head> <title> Finding Easter </title> <meta charset="utf-8"></head>')
 print('<body>')
-print('<p>')
-print('Easter falls on' & str(p) & "/" & str(n) & "/" & str(x))
-            #prints day/month/year e.g. 25/12/2018
+print('<h1 style="margin-left: 1.5%; margin-right: 1.5%; color: #ffffff; background-color: #7289da; border: thin groove #99aab5; text-align: center;"> Finding Easter </h1>')
+print('<p style="font-family: verdana, verdana sans-serif; margin-left: 2%; margin-right: 2%; color: #ffffff; background-color: #2c2f33;">')
+
+print('<fieldset style="font-family: verdana, verdana sans-serif; margin-left: 2%; margin-right: 2%; color: #ffffff; background-color: #2c2f33;">')
+print('<legend>Easter</legend>')
+print('<br>')
+if format == 'num':
+    print('Easter falls on: ' + str(p) + "/" + str(n) + "/" + str(x))
+                #   prints the date of easter numerically
+                #   e.g. day/month/year e.g. 25/12/2018
+elif format == 'verb':
+    print('Easter falls on the ' + str(p)+'<sup>'+sup+'</sup>' + " of " + month + " " + str(x))
+                #   prints the date of easter Verbosely
+                #   e.g 25th of December 2018
+elif format == 'both':
+    print('Easter falls on: ' + str(p) + "/" + str(n) + "/" + str(x))
+    print('<br>')
+    print('Easter falls on the ' + str(p)+'<sup>'+sup+'</sup>' + " of " + month + " " + str(x))
+                #   prints the date of easter in both formats
+                #   e.g. 25/12/2018 & 25th of December 2018
+print('<br>')
+print('</fieldset>')
+
 print('</p>')
 print('</body>')
 print('</html>')
